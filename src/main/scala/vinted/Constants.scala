642533fe6f43ee1250add3fa1db9608b900429bb
Mathieu <mathieu.leclaire@iscpif.fr>
2023-11-15 16:15:01 +0100

    Implements sales

--- src/main/scala/vinted/Constants.scala
+++ src/main/scala/vinted/Constants.scala
@@ -1,11 +1,12 @@
 package vinted
 
 object Constants:
-  
+
   type KG_CO2 = Int
   type KM = Int
   type EURO = Int
   type NB_ITEMS = Int
-  
-  
+
   val KG_CO2_PER_ITEM_PRODUCTION = 15.0
+  val ATTRACTIVENESS_FOR_SELLERS = 0.01
+  val ATTRACTIVENESS_FOR_BUYERS = 0.01
--- src/main/scala/vinted/Simulation.scala
+++ src/main/scala/vinted/Simulation.scala
@@ -1,35 +1,49 @@
 package vinted
 
-import vinted.Constants.{EURO, KM}
+import vinted.Constants.{ATTRACTIVENESS_FOR_BUYERS, ATTRACTIVENESS_FOR_SELLERS, EURO, KM}
 
 type States = Array[MonthlyState]
 case class Reinvestment(`new`: Double, platform: Double, elsewhere: Double) // sum is 1
 
 
 case class Simulation(
-                        initialSellers: Int,
-                        initialBuyers: Int,
-                        itemsBySellerByMonth: Double,
-                        itemsByBuyerByMonth: Double,
-                        transportCO2Intensity: TransportCO2Intensity,
-                        averageDistance: KM,
-                        averagePrice: EURO,
-                        replacementRatio: Double,
-                        impulsiveRatio: Double,
-                        reinvestment: Reinvestment,
+      initialSellers: Int,
+      initialBuyers: Int,
+      itemsBySellerByMonth: Double,
+      itemsByBuyerByMonth: Double,
+      transportCO2Intensity: TransportCO2Intensity,
+      averageDistance: KM,
+      averagePrice: EURO,
+      replacementRatio: Double,
+      impulsiveRatio: Double,
+      reinvestment: Reinvestment,
+      effectiveSalesCoefficient: Double
 
   )
 
 object Simulation:
 
-  def iterate(states: States): States = {
-    if (states.size == 100) states
-    else
-      val previousMonthState = states.last
-      previousMonthState.sel
+  def run(simulation: Simulation) =
+
+    def iterate(states: States): States = {
+      if (states.size == 100) states
+      else
+        val previousMonthState = states.last
+
+        val itemsForSale = previousMonthState.sellers * simulation.itemsBySellerByMonth
+        val purchaseIntention = previousMonthState.buyers * simulation.itemsByBuyerByMonth
+
+        val sales = Math.min(itemsForSale, purchaseIntention) * simulation.effectiveSalesCoefficient
+
+        val totalSellers = previousMonthState.sellers + sales * ATTRACTIVENESS_FOR_SELLERS
+        val totalBuyers = previousMonthState.buyers + itemsForSale * ATTRACTIVENESS_FOR_BUYERS
 
       val monthlyState = MonthlyState(
+          sellers = previousMonthState.sellers + 0.01 * sales
 
-      )
-      iterate(states :+ monthlyState)
-  }
+
+        )
+        iterate(states :+ monthlyState)
+    }
+
+    //iterate()
