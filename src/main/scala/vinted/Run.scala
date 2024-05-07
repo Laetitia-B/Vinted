d82bda8067237270a462081977b98882ee24d5e7
Mathieu <mathieu.leclaire@iscpif.fr>
2024-05-07 17:10:09 +0200

    Update SwitchExplorer

--- src/main/scala/vinted/Run.scala
+++ src/main/scala/vinted/Run.scala
@@ -2,36 +2,39 @@ package vinted
 
 
 object Run extends App:
+  
+  val simulation =
+    Simulation(
+      initialSellers = 30,
+      initialBuyers = 15,
+      initialItemsBySeller = 1.66,
+      initialItemsByBuyer = 1.0,
+      maximumItemsBySeller = 2.0,
+      maximumItemsBySellerDelay = 50,
+      maximumItemsByBuyer = 1.0,
+      maximumItemsByBuyerDelay = 50,
+      transportCO2Intensity = strategy.TransportCO2Intensity.Constant(0.01),
+      averageDistance = 150,
+      averagePrice = 20,
+      replacementRatio = 0.39, //sum of replacement/impulsive < 1
+      impulsiveRatio = 0.3,
+      secondHandItemsRatio = 0.72, // sum of secondHand and newItems is 1.0
+      reinvestementInNewRatio = 0.09, //sum of reinvestments is 1
+      reinvestementInPlatformRatio = 0.61,
+      effectiveSalesCoefficient = 0.85,
+      attractivenessForSellersDelay = 4,
+      attractivenessForBuyersDelay = 3,
+      attractivenessForSellers = 0.01,
+      attractivenessForBuyers = 0.01,
+      reinvestmentDelay = 6
+    )
+    
   switchExplorer
+  //default
   
   def default = 
     println("Vinted !")
-    val simulation =
-      Simulation(
-        initialSellers = 30,
-        initialBuyers = 15,
-        initialItemsBySeller = 1.66,
-        initialItemsByBuyer = 1.0,
-        maximumItemsBySeller = 2.0,
-        maximumItemsBySellerDelay = 50,
-        maximumItemsByBuyer = 1.0,
-        maximumItemsByBuyerDelay = 50,
-        transportCO2Intensity = strategy.TransportCO2Intensity.Constant(0.01),
-        averageDistance = 150,
-        averagePrice = 20,
-        replacementRatio = 0.39, //sum of replacement/impulsive < 1
-        impulsiveRatio = 0.3,
-        secondHandItemsRatio = 0.72, // sum of secondHand and newItems is 1.0
-        reinvestementInNewRatio = 0.09, //sum of reinvestments is 1
-        reinvestementInPlatformRatio = 0.61,
-        effectiveSalesCoefficient = 0.85,
-        attractivenessForSellersDelay = 4,
-        attractivenessForBuyersDelay = 3,
-        attractivenessForSellers = 0.01,
-        attractivenessForBuyers = 0.01,
-        reinvestmentDelay = 6
-      )
-  
+    
     val states = Simulation.run(simulation)
     MonthlyState.print(states.last)
   
@@ -40,6 +43,6 @@ object Run extends App:
 
   def switchExplorer =
     println("Switch explorer !")
-    SwitchExplorer.explore("/tmp/vinted")
+    SwitchExplorer.explore("/tmp/vinted", simulation)
     
     
--- src/main/scala/vinted/SwitchExplorer.scala
+++ src/main/scala/vinted/SwitchExplorer.scala
@@ -6,7 +6,7 @@ import better.files.File
 
 object SwitchExplorer {
 
-  def explore(outputPath: String) =
+  def explore(outputPath: String, simulation: Simulation) =
 
     val switchers =
       Seq(
@@ -24,36 +24,10 @@ object SwitchExplorer {
         Some(Switcher(SwitchType.AttractivenessForBuyers(0.005), 20)),
         Some(Switcher(SwitchType.ReinvestmentDelay(3), 20))
       )
-
-    val initialSimulation =
-      Simulation(
-        initialSellers = 30,
-        initialBuyers = 15,
-        initialItemsBySeller = 1.66,
-        initialItemsByBuyer = 1.0,
-        maximumItemsBySeller = 2.0,
-        maximumItemsBySellerDelay = 50,
-        maximumItemsByBuyer = 1.0,
-        maximumItemsByBuyerDelay = 50,
-        transportCO2Intensity = strategy.TransportCO2Intensity.Constant(0.01),
-        averageDistance = 150,
-        averagePrice = 20,
-        replacementRatio = 0.39, //sum of replacement/impulsive < 1
-        impulsiveRatio = 0.3,
-        secondHandItemsRatio = 0.72, // sum of secondHand and newItems is 1.0
-        reinvestementInNewRatio = 0.09, //sum of reinvestments is 1
-        reinvestementInPlatformRatio = 0.61,
-        effectiveSalesCoefficient = 0.85,
-        attractivenessForSellersDelay = 4,
-        attractivenessForBuyersDelay = 3,
-        attractivenessForSellers = 0.01,
-        attractivenessForBuyers = 0.01,
-        reinvestmentDelay = 6
-      )
-
+    
     val dynamics = switchers map : s =>
       println("Run " + s.map(_.switchType.toString).getOrElse("Base"))
-      val states = Simulation.run(initialSimulation, s)
+      val states = Simulation.run(simulation, s)
 
       Seq(
         states.map(_.sales),
